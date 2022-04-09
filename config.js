8bcafd191100ae66a088fa2e1b2e4af7850a01cb
Chiraitori <77224364+tathaha@users.noreply.github.com>
2022-04-09 20:54:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     "token": "process.env.TOKEN",
-    "prefix": "",
-    "MEMER_API_TOKEN": "", // Get The Memer API Token from https://discord.com/invite/emD44ZJaSA
-    "TENOR_API_KEY": "", // Get The Tenor API Key from https://www.tenor.co/api/v1/key/
-    "mongooseConnectionString": "", 
+    "prefix": "r!",
+    "MEMER_API_TOKEN": "FV0yw2iHBZM", // Get The Memer API Token from https://discord.com/invite/emD44ZJaSA
+    "TENOR_API_KEY": "5CZESJ8PDKBU", // Get The Tenor API Key from https://www.tenor.co/api/v1/key/
+    "mongooseConnectionString": "mongodb+srv://chiraitori:<password>@cluster0.9ha2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
     "id": "process.env.ID",  // https://discordapp.com/developers/applications/ID/information,
     "clientSecret": "process.env.SECRET",  // https://discordapp.com/developers/applications/ID/information,
-    "domain": "http://localhost:3000",
+    "domain": "https://discordbot-flash-production.up.railway.app",
     "port": 3000,
     "usingCustomDomain": true
 }
